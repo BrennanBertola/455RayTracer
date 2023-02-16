1651976b3f4c341c705ee71ba79a54e48818f4cc
BrennanBertola <brennan.bertola@comcast.net>
2023-02-16 16:42:30 -0700

    camera class implemented

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -33,12 +33,6 @@ void RayTracer::traceScene(int imgW, int imgH, Scene* scene) {
     }
 
     Camera cam = scene->getCam();
-    vec3 uS = vec3(1,0,0);
-    vec3 vS = vec3(0,1,0);
-    auto horizontal = vec3(cam.max.first*2, 0, 0);
-    auto vertical = vec3(0, cam.max.second*2, 0);
-    auto lower_left_corner = cam.getLookFrom() - horizontal/2 - vertical/2 - vec3(0, 0, 0);
-
     os << "P3\n" << imgW << " " << imgH << "\n255\n";
 
     for (int j = imgH-1; j >= 0; --j) {
@@ -46,12 +40,7 @@ void RayTracer::traceScene(int imgW, int imgH, Scene* scene) {
         for (int i = 0; i < imgW; ++i) {
             float u = double(i) * ((cam.max.first - cam.min.first)/(imgW)) + cam.min.first;
             float v = double(j) * ((cam.max.second - cam.min.second)/(imgH)) + cam.min.second;
-            Ray r(cam.getLookFrom(),  u*uS + v*vS + cam.getLookAt() - cam.getLookFrom());
-
-//            auto u = double(i) / (imgW-1);
-//            auto v = double(j) / (imgH-1);
-//            Ray r(cam.getLookFrom(), lower_left_corner + u*horizontal + v*vertical - cam.getLookFrom());
-
+            Ray r(cam.getLookFrom(),  u*vec3(1,0,0) + v*vec3(0,1,0) + cam.getLookAt() - cam.getLookFrom());
             vec3 pixel_color = ray_color(r, scene);
             write_color(os, pixel_color);
         }
