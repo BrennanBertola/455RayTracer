03e3649330df365af3191c017cd8286ef6b68514
BrennanBertola <brennan.bertola@comcast.net>
2023-02-22 16:01:09 -0700

    fixed lighting

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -23,18 +23,6 @@ int main() {
     tracer.traceScene(image_width, image_height, scene);
     delete scene;
 
-    scene = new Scene (vec3(.2,.2,.2), vec3(0,0,0), "12");
-    scene->setCam(cam);
-
-    light = Light(vec3(0,1,0), vec3(1,1,1));
-    scene->setLight(light);
-
-    sphere = new Sphere (vec3(0,0,-1), .4, .7, .2, .1, vec3(1,0,1), vec3(1,1,1), 16);
-    scene->addObj(sphere);
-
-    tracer.traceScene(image_width, image_height, scene);
-    delete scene;
-
     scene = new Scene (vec3(.2,.2,.2), vec3(.1,0.1,0.1), "2");
     scene->setCam(cam);
 
@@ -53,7 +41,7 @@ int main() {
     tracer.traceScene(image_width, image_height, scene);
     delete scene;
 
-    scene = new Scene (vec3(.2,.2,.2), vec3(.1,0.1,0.1), "3");
+    scene = new Scene (vec3(.2,.2,.7), vec3(.5,.5,0.5), "3");
     scene->setCam(cam);
 
     light = Light(vec3(0,1,1), vec3(1,1,1));
